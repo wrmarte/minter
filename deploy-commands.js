75683c35cf240978afcaa0f9e35dcd0ec1bb08a2
wrmarte <61038019+wrmarte@users.noreply.github.com>
2025-05-20 19:30:09 -0400

    Create deploy-commands.js

---
+++ deploy-commands.js
@@ -0,0 +1,32 @@
+const { REST, Routes } = require('discord.js');
+const fs = require('fs');
+require('dotenv').config();
+
+const commands = [];
+const commandFiles = fs.readdirSync('./minter/commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+  const command = require(`./minter/commands/${file}`);
+  if ('data' in command && 'execute' in command) {
+    commands.push(command.data.toJSON());
+  } else {
+    console.warn(`[⚠️] The command at ${file} is missing required "data" or "execute" property.`);
+  }
+}
+
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+
+(async () => {
+  try {
+    console.log('🔁 Started refreshing application (/) commands...');
+
+    await rest.put(
+      Routes.applicationCommands(process.env.CLIENT_ID),
+      { body: commands }
+    );
+
+    console.log('✅ Successfully reloaded application (/) commands!');
+  } catch (error) {
+    console.error('❌ Error while reloading commands:', error);
+  }
+})();
