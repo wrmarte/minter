f670466545a88a1ef0ca88233a66ab6afed56caf
wrmarte <61038019+wrmarte@users.noreply.github.com>
2025-05-20 19:35:59 -0400

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -3,10 +3,10 @@ const fs = require('fs');
 require('dotenv').config();
 
 const commands = [];
-const commandFiles = fs.readdirSync('./minter/commands').filter(file => file.endsWith('.js'));
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
-  const command = require(`./minter/commands/${file}`);
+  const command = require(`./commands/${file}`);
   if ('data' in command && 'execute' in command) {
     commands.push(command.data.toJSON());
   } else {
