8105cfcea72b12ef2521cf9357036c4eac592d86
wrmarte <61038019+wrmarte@users.noreply.github.com>
2025-05-20 19:24:54 -0400

    Update registerCommands.js

--- registerCommands.js
+++ registerCommands.js
@@ -3,6 +3,7 @@ const { REST, Routes } = require('discord.js');
 const fs = require('fs');
 const path = require('path');
 
+
 const commands = [];
 const commandFiles = fs.readdirSync(path.join(__dirname, 'commands')).filter(file => file.endsWith('.js'));
 
