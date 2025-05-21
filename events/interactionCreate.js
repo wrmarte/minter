d878ea4955837efbf6826f8705e560a18d9ada7c
wrmarte <61038019+wrmarte@users.noreply.github.com>
2025-05-20 22:32:43 -0400

    Update interactionCreate.js

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -6,13 +6,12 @@ module.exports = async (interaction, commands, context) => {
 
   try {
     await command.execute(interaction, context);
-  } catch (error) {
-    console.error(`❌ Error running command /${interaction.commandName}:`, error);
+  } catch (err) {
+    console.error(`❌ Error executing /${interaction.commandName}:`, err);
     if (interaction.replied || interaction.deferred) {
-      await interaction.followUp({ content: '❌ Something went wrong.', ephemeral: true });
+      await interaction.editReply({ content: '❌ Error running command.' });
     } else {
-      await interaction.reply({ content: '❌ Something went wrong.', ephemeral: true });
+      await interaction.reply({ content: '❌ Failed to run command.', ephemeral: true });
     }
   }
 };
-
