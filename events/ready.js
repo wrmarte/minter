67c601152601da38904631b4959ebb944d1d5517
wrmarte <61038019+wrmarte@users.noreply.github.com>
2025-05-22 22:30:40 -0400

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,15 +1,18 @@
 const { trackAllContracts } = require('../services/trackContracts');
-
-
+const trackTokenSales = require('../services/trackTokenSales');
 
 module.exports = client => {
   client.once('ready', async () => {
     console.log(`✅ Logged in as ${client.user.tag}`);
+
     const res = await client.pg.query(`SELECT * FROM contract_watchlist`);
     for (const row of res.rows) {
       await trackAllContracts(client, row);
     }
+
+    await trackTokenSales(client); // 👈 added token sale tracking
   });
 };
+
 
 
