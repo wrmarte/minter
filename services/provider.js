1aaa7ec9b0776015105802d143179e253dc0135f
wrmarte <61038019+wrmarte@users.noreply.github.com>
2025-05-31 12:30:47 -0400

    Update provider.js

--- services/provider.js
+++ services/provider.js
@@ -1,1 +1,25 @@
+const { JsonRpcProvider } = require('ethers');
+
+const baseRpcs = [
+  'https://mainnet.base.org',
+  'https://base.publicnode.com',
+  'https://1rpc.io/base',
+  'https://base.llamarpc.com',
+  'https://base.meowrpc.com'
+];
+
+let currentRpcIndex = 0;
+let provider = new JsonRpcProvider(baseRpcs[currentRpcIndex]);
+
+function rotateProvider() {
+  currentRpcIndex = (currentRpcIndex + 1) % baseRpcs.length;
+  console.warn(`🔁 Switching to fallback Base RPC: ${baseRpcs[currentRpcIndex]}`);
+  provider = new JsonRpcProvider(baseRpcs[currentRpcIndex]);
+}
+
+function getProvider() {
+  return provider;
+}
+
+module.exports = { getProvider, rotateProvider };
 
