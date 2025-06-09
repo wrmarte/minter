0d6aa445d87bd9dc09749e222c5d009c4099fc14
wrmarte <61038019+wrmarte@users.noreply.github.com>
2025-06-09 16:58:05 -0400

    Update registerCommands.js

--- registerCommands.js
+++ registerCommands.js
@@ -8,6 +8,7 @@ const commands = [];
 const commandsPath = path.join(__dirname, 'commands');
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
+
 for (const file of commandFiles) {
   const filePath = path.join(commandsPath, file);
   const command = require(filePath);
