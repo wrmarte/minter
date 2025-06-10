73b0caab73296508e279d60fdd8f2cb7d53377f2
wrmarte <61038019+wrmarte@users.noreply.github.com>
2025-06-09 23:07:03 -0400

    Create rpcList.js

---
+++ utils/rpcList.js
@@ -0,0 +1,19 @@
+const RPCS = {
+  base: [
+    'https://mainnet.base.org',
+    'https://base.publicnode.com',
+    'https://1rpc.io/base',
+    'https://base.llamarpc.com'
+  ],
+  eth: [
+    'https://eth.llamarpc.com',
+    'https://1rpc.io/eth',
+    'https://rpc.ankr.com/eth'
+  ],
+  ape: [
+    'https://apechain.drpc.org',
+    'https://rpc.apechain.com'
+  ]
+};
+
+module.exports = { RPCS };
