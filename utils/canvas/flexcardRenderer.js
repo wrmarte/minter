fb461fee7fedfdb576ba8e0495776cb03075ae52
wrmarte <61038019+wrmarte@users.noreply.github.com>
2025-06-11 09:20:33 -0400

    Update flexcardRenderer.js

--- utils/canvas/flexcardRenderer.js
+++ utils/canvas/flexcardRenderer.js
@@ -2,7 +2,6 @@ const { createCanvas, loadImage, GlobalFonts } = require('@napi-rs/canvas');
 const QRCode = require('qrcode');
 const path = require('path');
 
-// Load font
 const fontPath = path.join(__dirname, '../../fonts/Exo2-Bold.ttf');
 GlobalFonts.registerFromPath(fontPath, 'Exo2');
 
@@ -19,32 +18,30 @@ async function generateFlexCard({
   const canvas = createCanvas(width, height);
   const ctx = canvas.getContext('2d');
 
-  // Background color
+  // Green background + outer border
   ctx.fillStyle = '#31613D';
   ctx.fillRect(0, 0, width, height);
-
-  // Outer white border
-  ctx.strokeStyle = '#fff';
   ctx.lineWidth = 2;
+  ctx.strokeStyle = '#fff';
   ctx.strokeRect(0, 0, width, height);
 
-  // Header Title Bar
+  // Title Bar
   ctx.fillStyle = '#000';
-  ctx.fillRect(40, 40, width - 80, 60);
-  ctx.strokeStyle = '#fff';
-  ctx.strokeRect(40, 40, width - 80, 60);
-  ctx.font = 'bold 38px Exo2';
+  ctx.fillRect(40, 40, width - 80, 80);
+  ctx.strokeRect(40, 40, width - 80, 80);
+  ctx.font = 'bold 42px Exo2';
   ctx.fillStyle = '#fff';
-  ctx.fillText(`${collectionName.toUpperCase()} #${tokenId}`, 60, 82);
+  ctx.fillText(`${collectionName.toUpperCase()} #${tokenId}`, 60, 95);
 
-  // NFT Image box
+  // NFT Image box (centered)
   const imgX = 112;
-  const imgY = 130;
+  const imgY = 140;
   const imgSize = 900;
-  ctx.drawImage(await loadImage(nftImageUrl), imgX, imgY, imgSize, imgSize);
+  const nftImg = await loadImage(nftImageUrl);
+  ctx.drawImage(nftImg, imgX, imgY, imgSize, imgSize);
   ctx.strokeRect(imgX, imgY, imgSize, imgSize);
 
-  // Vertical OWNER tag
+  // OWNER vertical tag
   ctx.save();
   ctx.translate(width - 35, imgY + imgSize / 2);
   ctx.rotate(-Math.PI / 2);
@@ -53,52 +50,56 @@ async function generateFlexCard({
   ctx.fillText('OWNER', -50, 0);
   ctx.restore();
 
-  // Traits Box
+  // Traits + QR Container
   const traitsY = imgY + imgSize + 30;
-  const traitsBoxHeight = 300;
+  const boxHeight = 320;
   ctx.fillStyle = '#000';
-  ctx.fillRect(40, traitsY, width - 80, traitsBoxHeight);
+  ctx.fillRect(40, traitsY, width - 80, boxHeight);
   ctx.strokeStyle = '#fff';
-  ctx.strokeRect(40, traitsY, width - 80, traitsBoxHeight);
+  ctx.strokeRect(40, traitsY, width - 80, boxHeight);
 
-  ctx.font = 'bold 28px Exo2';
+  // "TRAITS" header
+  ctx.font = 'bold 30px Exo2';
   ctx.fillStyle = '#fff';
   ctx.fillText('TRAITS', 60, traitsY + 40);
 
+  // Traits list
   ctx.font = '24px Exo2';
-  let y = traitsY + 80;
-  for (let i = 0; i < Math.min(traits.length, 7); i++) {
-    ctx.fillText(traits[i], 60, y);
-    y += 30;
+  let traitY = traitsY + 80;
+  const maxTraits = 7;
+  for (let i = 0; i < Math.min(traits.length, maxTraits); i++) {
+    ctx.fillText(traits[i], 60, traitY);
+    traitY += 30;
   }
-  if (traits.length > 7) {
-    ctx.fillText(`+ ${traits.length - 7} more...`, 60, y);
+  if (traits.length > maxTraits) {
+    ctx.fillText(`+ ${traits.length - maxTraits} more...`, 60, traitY);
   }
 
   // QR Code
-  const qrSize = 240;
+  const qrSize = 220;
   const qrBuffer = await QRCode.toBuffer(openseaUrl, { width: qrSize });
   const qrImg = await loadImage(qrBuffer);
-  ctx.drawImage(qrImg, width - qrSize - 60, traitsY + 40, qrSize, qrSize);
-  ctx.strokeRect(width - qrSize - 60, traitsY + 40, qrSize, qrSize);
+  const qrX = width - qrSize - 80;
+  const qrY = traitsY + 50;
+  ctx.drawImage(qrImg, qrX, qrY, qrSize, qrSize);
+  ctx.strokeRect(qrX, qrY, qrSize, qrSize);
 
   // Footer
   const footerY = height - 60;
   ctx.fillStyle = '#000';
   ctx.fillRect(40, footerY, width - 80, 40);
-  ctx.strokeStyle = '#fff';
   ctx.strokeRect(40, footerY, width - 80, 40);
-
   ctx.font = 'bold 22px Exo2';
   ctx.fillStyle = '#fff';
   const footerText = 'Powered by PimpsDev 🚀';
   const textWidth = ctx.measureText(footerText).width;
-  ctx.fillText(footerText, (width - textWidth) / 2, footerY + 28);
+  ctx.fillText(footerText, (width - textWidth) / 2, footerY + 27);
 
   return canvas.toBuffer('image/png');
 }
 
 module.exports = { generateFlexCard };
+
 
 
 
