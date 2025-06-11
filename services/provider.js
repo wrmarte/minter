e789876350d4460d669a46fc2141b4f016151959
wrmarte <61038019+wrmarte@users.noreply.github.com>
2025-06-10 20:43:08 -0400

    Update provider.js

--- services/provider.js
+++ services/provider.js
@@ -1,6 +1,25 @@
 const { JsonRpcProvider } = require('ethers');
-const provider = new JsonRpcProvider('https://mainnet.base.org');
-module.exports = provider;
+
+const RPCS = {
+  base: [
+    'https://mainnet.base.org',
+    'https://base.publicnode.com',
+    'https://1rpc.io/base',
+    'https://base.llamarpc.com'
+  ]
+};
+
+let rpcIndex = 0;
+
+function getProvider(chain = 'base') {
+  const urls = RPCS[chain];
+  const url = urls[rpcIndex];
+  rpcIndex = (rpcIndex + 1) % urls.length;
+  return new JsonRpcProvider(url);
+}
+
+module.exports = { getProvider };
+
 
 
 
