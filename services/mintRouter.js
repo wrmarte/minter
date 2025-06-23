21d631574ad823248d041190eed84334f8adff3a
wrmarte <61038019+wrmarte@users.noreply.github.com>
2025-06-22 21:48:30 -0400

    Update mintRouter.js

--- services/mintRouter.js
+++ services/mintRouter.js
@@ -2,14 +2,12 @@ const { trackBaseContracts } = require('./mintProcessorBase');
 const { trackEthContracts } = require('./mintProcessorEth');
 const { trackApeContracts } = require('./mintProcessorApe');
 
-
 function trackAllContracts(client) {
   trackBaseContracts(client);
   trackEthContracts(client);
-  // trackApeContracts(client);
+  trackApeContracts(client); // ✅ Make sure this is active
 }
 
 module.exports = {
   trackAllContracts
 };
-
