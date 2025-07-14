11b9ec1d731ec20c1d4419d137c073530442ad16
wrmarte <61038019+wrmarte@users.noreply.github.com>
2025-07-14 00:34:12 -0400

    Update floppyRenderer.js

--- utils/canvas/floppyRenderer.js
+++ utils/canvas/floppyRenderer.js
@@ -16,7 +16,7 @@ async function buildFloppyCard(contractAddress, tokenId, collectionName, chain, 
 
     ctx.drawImage(floppyImage, 0, 0, 600, 600);
     ctx.drawImage(nftImage, 100, 50, 260, 260); // Bigger NFT top left inside label
-    ctx.drawImage(qrImage, 395, 260, 85, 85); // QR bottom right inside label
+    ctx.drawImage(qrImage, 395, 270, 85, 85); // QR bottom right inside label
 
     ctx.font = 'bold 18px Arial';
     ctx.fillStyle = '#111';
